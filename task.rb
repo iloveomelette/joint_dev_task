b058e9ac0fe248428c46e89abcdd282ade3eea08
iloveomelette <mizuki1998hosei@gmail.com>
2022-02-09 21:22:01 +0900

    Q10~Q15を解答致しました

--- task.rb
+++ task.rb
@@ -107,14 +107,18 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  sports.flatten!.uniq!
+  puts "ユーザーの趣味一覧"
+  sports.each.with_index(1) do |sport, i|
+    puts "No#{i} #{sport}"
+  end
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  p data[:user][:name]
 end
 
 def q13
@@ -122,14 +126,15 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data.update(update_data)
+  p user_data
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  p data.keys
 end
 
 def q15
@@ -137,7 +142,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  data1.key?(:age) ? (puts "OK") : (puts "NG")
+  data2.key?(:age) ? (puts "OK") : (puts "NG")
 end
 
 def q16
