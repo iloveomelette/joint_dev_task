12bc950953e686cf8805ba7a72396a52a2cf763d
iloveomelette <mizuki1998hosei@gmail.com>
2022-02-24 09:14:53 +0900

    Q18を解答致しました。

--- task.rb
+++ task.rb
@@ -189,7 +189,14 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(user)
+    @name = user[:name]
+    @age = user[:age]
+  end
 
+  def introduce
+    @age >= 30 ? "こんにちは，#{@name}と申します。宜しくお願いいたします。" : "はいさいまいど〜，#{@name}です！！！"
+  end
 end
 
 def q18
